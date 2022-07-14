8ee03cd0b43361b6cb9ddf9e44407612ccd941c6
LEEJAEWON <graphixxx@amvient.com>
2022-07-14 10:46:37 +0900

    DOM을 이용하여 HTML 객체 가져오기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
 "use strict"
 
-console.log("hello login.js")
+const id_ = document.querySelector("#userid"),
+    psword_ = document.querySelector("#userpw"),
+    btnLogin_ = document.querySelector("#btn-login");
+
+btnLogin_.addEventListener('click', login);
+
+function login() {
+    const req = {
+        id: id_.value,
+        psword: psword_.value,
+    };
+
+    // to server
+
+};
